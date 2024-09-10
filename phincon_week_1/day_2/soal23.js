f808ead7e53e6f6d0028bc887306abab4f7cf0c1
Fioreza Radhin Naufal <fioreza1902@gmail.com>
2024-09-10 12:07:16 +0700

    basic js

--- phincon_week_1/day_2/soal23.js
+++ phincon_week_1/day_2/soal23.js
@@ -1,16 +1,16 @@
-function hitungKataUnik(kalimat) {
-  let kataSplit = kalimat.split("");
-  let count = 0;
-  //   looping kata dari kalimat yang di split
-  for (let i = 0; i < kalimat.length; i++) {
-    //   jika kata sama dengan kata yang di split
-    if (kataSplit[i] === kataSplit[i]) {
-      //   tambah count
-      count++;
-    }
-  }
-  console.log(count);
-}
+// function hitungKataUnik(kalimat) {
+//   let kataSplit = kalimat.split("");
+//   let count = 0;
+//   //   looping kata dari kalimat yang di split
+//   for (let i = 0; i < kalimat.length; i++) {
+//     //   jika kata sama dengan kata yang di split
+//     if (kataSplit[i] === kataSplit[i]) {
+//       //   tambah count
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
 
-// Ekspektasi hasil:
-hitungKataUnik("Saya suka makan nasi suka minum air");
+// // Ekspektasi hasil:
+// hitungKataUnik("Saya suka makan nasi suka minum air");
