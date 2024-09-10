8587322dbb9ec51274de136ecfcb42acedda6cef
Fioreza Radhin Naufal <fioreza1902@gmail.com>
2024-09-10 16:02:46 +0700

    registration and lampu toggle

--- phincon_week_1/day_2/soal1.js
+++ phincon_week_1/day_2/soal1.js
@@ -1,6 +1,8 @@
 function hitungBilanganGanjil(n) {
   let bilangan = 0;
+  // looping digunakan untuk mengecek bilangan ganjil
   for (let i = 1; i <= n; i++) {
+    // i berisi 1 sampai n
     if (i % 2 !== 0) {
       bilangan++;
     }
