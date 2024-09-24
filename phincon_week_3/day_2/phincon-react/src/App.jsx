683ce0ad37a6e8fee34f808e289db303d3db38ea
Fioreza Radhin Naufal <fioreza1902@gmail.com>
2024-09-24 14:57:47 +0700

    add todo apps

---
+++ phincon_week_3/day_2/phincon-react/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Header } from "./components/Header";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [listTask, setListTask] = useState([]);
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const storedList = localStorage.getItem("listTask");
+    if (storedList) {
+      setListTask(JSON.parse(storedList));
+    }
+  }, []);
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const addTask = () => {
+    const updatedList = [...listTask, task];
+    setListTask(updatedList);
+    localStorage.setItem("listTask", JSON.stringify(updatedList));
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    const newListTask = listTask.filter((item, i) => {
+      return i !== index;
+    });
+
+    setListTask(newListTask);
+    localStorage.setItem("listTask", JSON.stringify(newListTask));
+  };
+
+  return (
+    <div className="app">
+      <Header />
+      <div className="h-screen flex justify-center items-center ">
+        <div
+          className={`rounded-md px-20 py-6 ${
+            theme === "light" ? "bg-slate-400 " : " bg-black"
+          }`}
+        >
+          <h1 className="text-3xl mb-10 text-white text-center">To-Do List</h1>
+          <div className="grid gap-6">
+            <button
+              className="bg-blue-600 p-4 rounded-md text-white font-bold"
+              onClick={changeTheme}
+            >
+              Switch to {theme === "light" ? "dark" : "light"} Theme
+            </button>
+            <div className="flex gap-4">
+              <input
+                className="p-6 rounded-lg"
+                type="text"
+                placeholder="Add a new task"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+              />
+              <button
+                className="bg-blue-600 p-4 rounded-md text-white font-bold"
+                onClick={addTask}
+              >
+                Add Task
+              </button>
+            </div>
+            <div className="grid gap-4" id="listTask">
+              {listTask.map((item, index) => (
+                <div
+                  key={index}
+                  className="bg-white p-4 rounded-xl text-left flex items-center justify-between"
+                >
+                  <h1 className="font-bold text-xl">{item}</h1>
+                  <button
+                    className="bg-red-600 p-4 rounded-md text-white"
+                    onClick={() => deleteTask(index)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
